e9b70b0c59e926b1ec18c347b45d76eb3cf4999f
NastiaKed <stasaked@gmail.com>
2025-12-06 21:50:58 +0200

    add: authentication API logic

---
+++ lib/data/services/auth_service.dart
@@ -0,0 +1,38 @@
+import 'package:cours_work/data/services/api_client.dart';
+import 'package:cours_work/data/services/local_storage.dart';
+import 'package:dio/dio.dart';
+
+class AuthService {
+  final Dio _dio = ApiClient.dio;
+
+  Future<Response<dynamic>> register({
+    required String username,
+    required String email,
+    required String password,
+  }) async {
+    return await _dio.post(
+      'auth/register',
+      data: {'username': username, 'email': email, 'password': password},
+    );
+  }
+
+  Future<Response<dynamic>> login({
+    required String username,
+    required String password,
+  }) async {
+    final response = await _dio.post(
+      'auth/login',
+      data: FormData.fromMap({'username': username, 'password': password}),
+      options: Options(contentType: Headers.formUrlEncodedContentType),
+    );
+
+    if (response.statusCode == 200 && response.data != null) {
+      final token = response.data['access_token'];
+
+      if (token != null) {
+        await LocalStorage.saveToken(token.toString());
+      }
+    }
+    return response;
+  }
+}
